89927a85bb9994a63dfb9ff12a3f4ac952efe681
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-12-17 23:17:18 +0100

    Added google map lat. and lng.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,14 @@
 import GoogleMapReact from 'google-map-react'
 
 const Map = ({center, zoom}) => {
-    retun( 
+    return( 
         <div className="map">
-            <GoogleMapReact bootstrapURLKeys={{key:'xxxxxxxxxxxxxxx'}} // API key
+            <GoogleMapReact bootstrapURLKeys={{key:
+            'xxxxxxx....'}} // API key
                 defaultCenter={center}
                 defaultZoom={zoom}
             >
             </GoogleMapReact> 
-
         </div>
     )
 }
@@ -18,7 +18,7 @@ Map.defaultProps = {
         lat: 42.3265, 
         lng: -122.8756
     },
-    zoom: 6; 
+    zoom: 6
 }
 
 export default Map
