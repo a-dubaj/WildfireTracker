7c20a2032f82c9ab00226f45841c6a47575cae91
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-12-21 15:32:46 +0100

    Fix problem with displey fire icon

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,12 +1,14 @@
+//import { Icon } from '@iconify/react';
 import { Icon } from '@iconify/react'
+//import locationIcon from '@iconify-icons/fire-alert'
 import locationIcon from '@iconify/icons-mdi/fire-alert'
 
 const LocationMarker = ({ lat, lng, onClick }) => {
-    return (
-        <div class="location-marker" onClick={onClick}>
-            <Icon icon={locationIcon} className="location-Icon" />
+    return ( < div class = "location-marker"
+        onClick = { onClick } >
+        <Icon icon = { locationIcon } className = "location-Icon" / >
         </div>
     )
-} 
+}
 
 export default LocationMarker
