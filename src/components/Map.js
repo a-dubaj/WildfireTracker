4aa5a982ee85eb221e60793a563a1c75c96efc64
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-12-21 18:09:42 +0100

    Added locatio info box and styling

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,27 +1,33 @@
+import {useState} from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
 const api_key = process.env.REACT_APP_API_KEY
 
 const Map = ({ eventData, center, zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
     const markers = eventData.map(e => {
-        if(e.categories[0].id === 8) {
-            return <LocationMarker lat = {e.geometries[0].coordinates[1]}lng = {e.geometries[0].coordinates[0]} />  
+        if (e.categories[0].id === 8) {
+            return <LocationMarker lat={e.geometries[0].coordinates[1]} lng={e.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: e.id, title: e.title })} />  
         }
         return null
     })
 
-    return ( < div className = "map" >
+    return ( <div className = "map">
         <GoogleMapReact bootstrapURLKeys = {
             {
                 key: api_key
             }
         }
             defaultCenter = { center }
-            defaultZoom = { zoom } >
-                {markers}
-            </GoogleMapReact> 
-        </div >
+            defaultZoom = { zoom }
+        > 
+        { markers } 
+        </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
+        </div>
     )
 }
 
