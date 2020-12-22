885261d7d311b804fb26bb0674f85351f1e46901
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2020-12-22 11:06:11 +0100

    Test changes

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,12 +1,10 @@
-//import { Icon } from '@iconify/react';
-import { Icon } from '@iconify/react'
-//import locationIcon from '@iconify-icons/fire-alert'
+import {Icon} from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/fire-alert'
 
 const LocationMarker = ({ lat, lng, onClick }) => {
-    return ( < div class = "location-marker"
-        onClick = { onClick } >
-        <Icon icon = { locationIcon } className = "location-Icon" / >
+    return ( 
+        <div class = "location-marker" onClick = {onClick}>
+            <Icon icon = { locationIcon } className = "location-Icon" />
         </div>
     )
 }
